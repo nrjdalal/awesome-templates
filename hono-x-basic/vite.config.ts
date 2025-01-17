7969eb96979809e692f0a67971efe4a727b656c0
Neeraj Dalal <admin@nrjdalal.com>
2025-01-17 12:12:30 +0530

    test: move to dir

--- hono-x-basic/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
-})
