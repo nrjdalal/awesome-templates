4be0f25d1489d3abad8d96dae49a6a4d995cfd7e
Neeraj Dalal <admin@nrjdalal.com>
2025-01-25 17:11:24 +0530

    fix: shadcn react 19

--- .github/.scripts/next.js-shadcn.sh
+++ .github/.scripts/next.js-shadcn.sh
@@ -1,4 +1,8 @@
 bunx create-next-app@latest awesomedir --ts --eslint --tailwind --src-dir --app --turbopack --import-alias "@/*"
 cd awesomedir
-bunx shadcn@latest init -d
-bunx shadcn@latest add -a
+bunx shadcn@latest init -d <<EOF
+
+EOF
+bunx shadcn@latest add -a <<EOF
+
+EOF
