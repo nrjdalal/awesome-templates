22c98c56aff21bcba272772da1c2d6cc64f8ab26
Neeraj Dalal <admin@nrjdalal.com>
2025-02-17 08:48:41 +0530

    refactor: json to fx

--- .github/.scripts/next.js-shadcn.sh
+++ .github/.scripts/next.js-shadcn.sh
@@ -8,11 +8,32 @@ bunx shadcn@latest add -a <<EOF
 EOF
 # custom best practices
 bun add -D @commitlint/cli @commitlint/config-conventional @ianvs/prettier-plugin-sort-imports lint-staged prettier prettier-plugin-tailwindcss simple-git-hooks sort-package-json
-bunx json -I -f package.json \
-  -e 'this.scripts.prepare="if [ -z \"$VERCEL_ENV\" ]; then simple-git-hooks; fi"' \
-  -e 'this["simple-git-hooks"]={"pre-commit":"npx lint-staged --verbose","commit-msg":"npx commitlint --edit $1"}' \
-  -e 'this["commitlint"]={"extends":["@commitlint/config-conventional"]}' \
-  -e 'this["lint-staged"]={"*":"prettier --write --ignore-unknown","package.json":"sort-package-json"}' \
-  -e 'this["prettier"]={"semi":false,"plugins":["@ianvs/prettier-plugin-sort-imports","prettier-plugin-tailwindcss"]}'
+echo "$(bunx fx package.json '{
+  ...x,
+  "scripts": {
+    ...x.scripts,
+    "prepare": "if [ -z \"$VERCEL_ENV\" ]; then simple-git-hooks; fi",
+  },
+  "simple-git-hooks": {
+    "pre-commit": "npx lint-staged --verbose",
+    "commit-msg": "npx commitlint --edit $1"
+  },
+  "commitlint": {
+    "extends": [
+      "@commitlint/config-conventional"
+    ]
+  },
+  "lint-staged": {
+    "*": "prettier --write --ignore-unknown",
+    "package.json": "sort-package-json"
+  },
+  "prettier": {
+    "plugins": [
+      "@ianvs/prettier-plugin-sort-imports",
+      "prettier-plugin-tailwindcss"
+    ],
+    "semi": false
+  },
+}')" >package.json
 bunx sort-package-json
 bunx prettier --write --ignore-unknown *
