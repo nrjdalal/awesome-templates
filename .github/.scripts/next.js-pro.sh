0e0bf2b2c77c0523f73c6f4c89ed19ee220f52ba
Neeraj Dalal <admin@nrjdalal.com>
2025-04-29 12:14:56 +0530

    chore: tweaks > next.js-pro.sh
    .github/.scripts/next.js-pro.sh

--- .github/.scripts/next.js-pro.sh
+++ .github/.scripts/next.js-pro.sh
@@ -1,7 +1,9 @@
 bunx create-next-app@latest --ts --eslint --tailwind --src-dir --app --turbopack --import-alias "@/*" awesomedir
 cd awesomedir
 bunx shadcn@latest init --base-color neutral -d
+# add better-auth
 bunx shadcn@latest add https://raw.githubusercontent.com/nrjdalal/the-next-starter/refs/heads/main/public/r/app-api-auth.json
+# add next-theme sonner tanstack-query
 bunx shadcn@latest add https://raw.githubusercontent.com/nrjdalal/the-next-starter/refs/heads/main/public/r/app-providers.json
 bunx shadcn@latest add -a -o
 bunx colorwindcss@latest
