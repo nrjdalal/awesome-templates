a6d30cad2d35355b4c6baca26231972094e95733
Neeraj Dalal <admin@nrjdalal.com>
2025-05-27 16:46:53 +0530

    chore: tweaks > next.js-basic.sh
    .github/.scripts/next.js-basic.sh

--- .github/.scripts/next.js-basic.sh
+++ .github/.scripts/next.js-basic.sh
@@ -1,2 +1,25 @@
 bunx create-next-app@latest --ts --tailwind --src-dir --app --turbopack --import-alias "@/*" awesomedir
 cd awesomedir
+cat <<EOF >src/app/page.tsx
+export default function Home() {
+  return (
+    <main className="min-h-dvh w-screen flex items-center justify-center flex-col gap-y-4 p-4">
+      <img
+        className="max-w-sm w-full"
+        src="https://tanstack.com/assets/splash-dark-8nwlc0Nt.png"
+        alt="TanStack Logo"
+      />
+      <h1>
+        <span className="line-through">Next.js</span> TanStack Start
+      </h1>
+      <a
+        className="bg-foreground text-background rounded-full px-4 py-1 hover:opacity-90"
+        href="https://tanstack.com/start/latest"
+        target="_blank"
+      >
+        Docs
+      </a>
+    </main>
+  )
+}
+EOF
