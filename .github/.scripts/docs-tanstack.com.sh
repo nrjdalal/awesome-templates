2374f4c1e2f88c8f648edd9c43859a64ff839df4
Neeraj Dalal <admin@nrjdalal.com>
2025-05-29 19:59:34 +0530

    chore: tweaks > docs-tanstack.com.sh
    .github/.scripts/docs-tanstack.com.sh

--- .github/.scripts/docs-tanstack.com.sh
+++ .github/.scripts/docs-tanstack.com.sh
@@ -1,6 +1,6 @@
 bunx gitpick https://github.com/TanStack/tanstack.com awesomedir
 cd awesomedir
-sed -i "s/const repo = 'tanstack/router'/const repo = 'nrjdalal/router'/g" src/libraries/router.tsx
-sed -i "s/const repo = 'tanstack/router'/const repo = 'nrjdalal/router'/g" src/libraries/start.tsx
+sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/router'/g" src/libraries/router.tsx
+sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/router'/g" src/libraries/start.tsx
 sed -i "s/latestBranch: 'main'/latestBranch: 'preview-alpha'/g" src/libraries/router.tsx
 sed -i "s/latestBranch: 'main'/latestBranch: 'preview-alpha'/g" src/libraries/start.tsx
