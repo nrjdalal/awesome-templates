263b7a2906db3d5ca5c66358f1aec18157fcb680
Neeraj Dalal <admin@nrjdalal.com>
2025-05-29 20:22:38 +0530

    chore: tweaks > docs-tanstack-router.sh
    .github/.scripts/docs-tanstack-router.sh

--- .github/.scripts/docs-tanstack-router.sh
+++ .github/.scripts/docs-tanstack-router.sh
@@ -1,3 +1,3 @@
-bunx gitpick https://github.com/TanStack/tanstack.com awesomedir
+bunx gitpick https://github.com/TanStack/router/tree/alpha awesomedir
 cd awesomedir
 bunx gitpick https://github.com/nrjdalal/router/blob/migrate-from-next-js-app-router/docs/start/framework/react/migrate-from-next-js.md docs/start/framework/react
