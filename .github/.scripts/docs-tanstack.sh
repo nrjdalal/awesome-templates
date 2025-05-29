7f7ba3e4041840d1e34108faecf12171bb540f41
Neeraj Dalal <admin@nrjdalal.com>
2025-05-29 19:04:19 +0530

    chore: tweaks > docs-tanstack.sh
    .github/.scripts/docs-tanstack.sh

--- .github/.scripts/docs-tanstack.sh
+++ .github/.scripts/docs-tanstack.sh
@@ -1,4 +1,2 @@
-bunx gitpick https://github.com/TanStack/tanstack.com awesomedir
-bunx gitpick https://github.com/TanStack/router/tree/alpha
-cd awesomedir
-pnpm i && pnpm run build
+bunx gitpick https://github.com/TanStack/tanstack.com awesomedir/tanstack.com
+bunx gitpick https://github.com/TanStack/router/tree/alpha awesomedir/router
