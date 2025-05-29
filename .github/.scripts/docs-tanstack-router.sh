613d1a4508bebfafa61cda71844377ffc649ebd6
Neeraj Dalal <admin@nrjdalal.com>
2025-05-29 20:34:53 +0530

    chore: tweaks > docs-tanstack-router.sh docs-tanstack.com.sh
    .github/.scripts/docs-tanstack-router.sh
    .github/.scripts/docs-tanstack.com.sh

--- .github/.scripts/docs-tanstack-router.sh
+++ .github/.scripts/docs-tanstack-router.sh
@@ -1,3 +1,24 @@
-bunx gitpick https://github.com/TanStack/router/tree/alpha awesomedir
+bunx gitpick https://github.com/TanStack/router/tree/alpha/docs awesomedir/docs
 cd awesomedir
 bunx gitpick https://github.com/nrjdalal/router/blob/migrate-from-next-js-app-router/docs/start/framework/react/migrate-from-next-js.md docs/start/framework/react
+jq '.sections |= (
+    .[:1]
+  + [
+      {
+        "label": "Guides",
+        "children": [],
+        "frameworks": [
+          {
+            "label": "react",
+            "children": [
+              {
+                "label": "Migrate from Next.js",
+                "to": "framework/react/migrate-from-next-js"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  + .[1:]
+)' docs/start/config.json >tmp && mv tmp docs/start/config.json
--- .github/.scripts/docs-tanstack.com.sh
+++ .github/.scripts/docs-tanstack.com.sh
@@ -1,6 +1,6 @@
 bunx gitpick https://github.com/TanStack/tanstack.com awesomedir
 cd awesomedir
-sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/router'/g" src/libraries/router.tsx
-sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/router'/g" src/libraries/start.tsx
-sed -i "s/latestBranch: 'main'/latestBranch: 'alpha-with-next-migration-doc'/g" src/libraries/router.tsx
-sed -i "s/latestBranch: 'main'/latestBranch: 'alpha-with-next-migration-doc'/g" src/libraries/start.tsx
+sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/awesome-templates'/g" src/libraries/router.tsx
+sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/awesome-templates'/g" src/libraries/start.tsx
+sed -i "s/docsRoot: 'docs\/router'/docsRoot: 'docs-apps\/docs-tanstack-router\/docs\/router'/g" src/libraries/router.tsx
+sed -i "s/docsRoot: 'docs\/start'/docsRoot: 'docs-apps\/docs-tanstack-router\/docs\/start'/g" src/libraries/start.tsx
