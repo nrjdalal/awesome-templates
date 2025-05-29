1c7fd97e49b7305dfc9ae6adc44c787a733d8219
Neeraj Dalal <admin@nrjdalal.com>
2025-05-29 19:29:20 +0530

    chore: tweaks > docs-tanstack.sh
    .github/.scripts/docs-tanstack.sh

--- .github/.scripts/docs-tanstack.sh
+++ .github/.scripts/docs-tanstack.sh
@@ -1,2 +1,5 @@
-bunx gitpick https://github.com/TanStack/tanstack.com awesomedir/tanstack.com
-bunx gitpick https://github.com/TanStack/router/tree/alpha awesomedir/router
+bunx gitpick https://github.com/TanStack/tanstack.com awesomedir
+cd awesomedir
+# need to change latestBranch: 'main' to latestBranch: 'alpha' in src/libraries/router.tsx and src/libraries/start.tsx using sed
+sed -i 's/latestBranch: "main"/latestBranch: "alpha"/g' src/libraries/router.tsx
+sed -i 's/latestBranch: "main"/latestBranch: "alpha"/g' src/libraries/start.tsx
