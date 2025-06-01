9202aad559b5521cc9690e9b2721403429713920
Neeraj Dalal <admin@nrjdalal.com>
2025-06-02 00:30:18 +0530

    chore: tweaks > tanstack-start.sh
    .github/.scripts/tanstack-start.sh

--- .github/.scripts/tanstack-start.sh
+++ .github/.scripts/tanstack-start.sh
@@ -123,15 +123,13 @@ function Component() {
   return (
     <main className="flex min-h-dvh w-screen flex-col items-center justify-center gap-y-4 p-4">
       <img
-        className="w-full max-w-sm"
+        className="aspect-square w-full max-w-sm"
         src="https://tanstack.com/assets/splash-dark-8nwlc0Nt.png"
         alt="TanStack Logo"
       />
-      <h1>
-        <span className="line-through">Next.js</span> TanStack Start
-      </h1>
+      <h1>TanStack Start</h1>
       <a
-        className="bg-foreground text-background rounded-full px-4 py-1 hover:opacity-90"
+        className="rounded-full border px-4 py-1 hover:opacity-90"
         href="https://tanstack.com/start/latest"
         target="_blank"
       >
