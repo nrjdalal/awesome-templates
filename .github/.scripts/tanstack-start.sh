f1750f4c5b19849f3830e7739e7aa14ada6c6dae
Neeraj Dalal <admin@nrjdalal.com>
2025-06-01 20:33:18 +0530

    chore: tweaks > tanstack-start.sh
    .github/.scripts/tanstack-start.sh

--- .github/.scripts/tanstack-start.sh
+++ .github/.scripts/tanstack-start.sh
@@ -1,2 +1,13 @@
 mkdir awesomedir && cd awesomedir
 bunx gitpick nrjdalal/next-to-start/blob/main/.gitignore
+bun add @tanstack/react-router@alpha @tanstack/react-start@alpha vite
+bun add -D @tailwindcss/vite tailwindcss vite-tsconfig-paths
+bunx fx package.json '{
+  ...x,
+  "type": "module",
+  "scripts": {
+    "dev": "vite dev",
+    "build": "vite build",
+    "start": "node .output/server/index.mjs"
+  },
+}' save
