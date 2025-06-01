235887c68b8e48b68015a08346d0f2ec6771e8cc
Neeraj Dalal <admin@nrjdalal.com>
2025-06-01 21:37:35 +0530

    chore: tweaks > tanstack-start.sh
    .github/.scripts/tanstack-start.sh

--- .github/.scripts/tanstack-start.sh
+++ .github/.scripts/tanstack-start.sh
@@ -3,7 +3,7 @@ bunx gitpick nrjdalal/next-to-start/blob/main/.gitignore
 bun add react react-dom && bun add -D @types/node @types/react @types/react-dom typescript
 bun add @tanstack/react-router@alpha @tanstack/react-start@alpha vite
 bun add -D @tailwindcss/vite tailwindcss vite-tsconfig-paths
-bun add -D @commitlint/cli @commitlint/config-conventional @ianvs/prettier-plugin-sort-imports lint-staged prettier prettier-plugin-tailwindcss simple-git-hooks sort-package-json
+bun add -D prettier prettier-plugin-tailwindcss
 cat <<EOF >vite.config.ts
 import tailwindcss from "@tailwindcss/vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
@@ -124,48 +124,9 @@ bunx fx package.json '{
     "dev": "vite dev",
     "build": "vite build",
     "start": "node .output/server/index.mjs",
-    "prepare": "npx simple-git-hooks",
-    "drizzle": "bun --env-file=.env.development drizzle-kit push",
-    "drizzle:prod": "bun --env-file=.env.production drizzle-kit push",
-    "studio": "bun --env-file=.env.development drizzle-kit studio",
-    "studio:prod": "bun --env-file=.env.production drizzle-kit studio",
-  },
-  "simple-git-hooks": {
-    "pre-commit": "npx lint-staged --verbose",
-    "commit-msg": "npx commitlint --edit $1"
-  },
-  "commitlint": {
-    "extends": [
-      "@commitlint/config-conventional"
-    ]
-  },
-  "lint-staged": {
-    "*": "prettier --write --ignore-unknown",
-    "package.json": "sort-package-json"
   },
   "prettier": {
-    "importOrder": [
-      "<BUILTIN_MODULES>",
-      "",
-      "^react/(.*)$|^react$",
-      "^@tanstack/(.*)$|^@tanstack$",
-      "",
-      "<THIRD_PARTY_MODULES>",
-      "",
-      "^@/types/(.*)$",
-      "^@/config/(.*)$",
-      "^@/lib/(.*)$",
-      "^@/hooks/(.*)$",
-      "^@/db/(.*)$",
-      "^@/components/ui/(.*)$",
-      "^@/components/(.*)$",
-      "^@/app/(.*)$",
-      "^@/routes/(.*)$",
-      "",
-      "^[./]"
-    ],
     "plugins": [
-      "@ianvs/prettier-plugin-sort-imports",
       "prettier-plugin-tailwindcss"
     ],
     "printWidth": 100,
