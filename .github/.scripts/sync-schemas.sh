b35430c4e9b8c76d95e90aeb23d43cb92bf5914c
Neeraj Dalal <admin@nrjdalal.com>
2025-06-02 18:10:35 +0530

    chore: tweaks > sync-schemas.sh
    .github/.scripts/sync-schemas.sh

--- .github/.scripts/sync-schemas.sh
+++ .github/.scripts/sync-schemas.sh
@@ -4,4 +4,4 @@ cd next.js-pro
 bun i && bunx @better-auth/cli generate --config src/lib/auth/index.ts -y
 bunx prettier --write auth-schema.ts
 # move auth-schema.ts to awesomedir/drizzle-better-auth.ts
-mv auth-schema.ts awesomedir/drizzle-better-auth.ts
+mv auth-schema.ts ../awesomedir/drizzle-better-auth.ts
