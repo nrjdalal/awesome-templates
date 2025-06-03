34735c8749d05f3df126ecc5a41d129a538ac533
Neeraj Dalal <admin@nrjdalal.com>
2025-06-03 11:42:17 +0530

    docs(guide): integrate better auth > docs-tanstack.com.sh
    .github/.scripts/docs-tanstack.com.sh

--- .github/.scripts/docs-tanstack.com.sh
+++ .github/.scripts/docs-tanstack.com.sh
@@ -1,32 +1,32 @@
 bunx gitpick https://github.com/nrjdalal/tanstack.com -b revamp awesomedir
 cd awesomedir
-sed -i "s/latestBranch: 'main'/latestBranch: 'alpha'/g" src/libraries/router.tsx
-sed -i "s/latestBranch: 'main'/latestBranch: 'alpha'/g" src/libraries/start.tsx
+# sed -i "s/latestBranch: 'main'/latestBranch: 'alpha'/g" src/libraries/router.tsx
+# sed -i "s/latestBranch: 'main'/latestBranch: 'alpha'/g" src/libraries/start.tsx
 ## router/start alpha docs
-# bunx gitpick https://github.com/TanStack/router/tree/alpha/docs router-docs
-# bunx gitpick https://github.com/TanStack/router/blob/main/docs/start/framework/react/migrate-from-next-js.md router-docs/start/framework/react
-# jq '.sections |= (
-#     .[:1]
-#   + [
-#       {
-#         "label": "Guides",
-#         "children": [],
-#         "frameworks": [
-#           {
-#             "label": "react",
-#             "children": [
-#               {
-#                 "label": "Migrate from Next.js",
-#                 "to": "framework/react/migrate-from-next-js"
-#               }
-#             ]
-#           }
-#         ]
-#       }
-#     ]
-#   + .[1:]
-# )' router-docs/start/config.json >tmp && mv tmp router-docs/start/config.json
-# sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/awesome-templates'/g" src/libraries/router.tsx
-# sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/awesome-templates'/g" src/libraries/start.tsx
-# sed -i "s/docsRoot: 'docs\/router'/docsRoot: 'docs-apps\/docs-tanstack.com\/router-docs\/router'/g" src/libraries/router.tsx
-# sed -i "s/docsRoot: 'docs\/start'/docsRoot: 'docs-apps\/docs-tanstack.com\/router-docs\/start'/g" src/libraries/start.tsx
+bunx gitpick https://github.com/TanStack/router/tree/alpha/docs router-docs
+bunx gitpick https://github.com/nrjdalal/router/blob/guide-to-integrate-better-auth/docs/start/framework/react/integrate-better-auth.md router-docs/start/framework/react
+jq '.sections |= (
+    .[:1]
+  + [
+      {
+        "label": "Guides",
+        "children": [],
+        "frameworks": [
+          {
+            "label": "react",
+            "children": [
+              {
+                "label": "Integrate Better Auth",
+                "to": "framework/react/integrate-better-auth"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  + .[1:]
+)' router-docs/start/config.json >tmp && mv tmp router-docs/start/config.json
+sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/awesome-templates'/g" src/libraries/router.tsx
+sed -i "s/const repo = 'tanstack\/router'/const repo = 'nrjdalal\/awesome-templates'/g" src/libraries/start.tsx
+sed -i "s/docsRoot: 'docs\/router'/docsRoot: 'docs-apps\/docs-tanstack.com\/router-docs\/router'/g" src/libraries/router.tsx
+sed -i "s/docsRoot: 'docs\/start'/docsRoot: 'docs-apps\/docs-tanstack.com\/router-docs\/start'/g" src/libraries/start.tsx
